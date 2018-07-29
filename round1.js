24f71b9461720b101f2f3526319775c650cbbfd9
newtmex <brightomex@gmail.com>
2018-07-29 08:37:52 +0100

    refactored the selection of random percentage, added some useful comments

--- round1.js
+++ round1.js
@@ -2,10 +2,14 @@ const log = console.log
 function random(start,end){//Returns a random number from start to end, both included
   return Math.random() * (end - start + 1) + start;
 }
-const rand = random.bind(null,4.4,9.09)
 
-var airDrT = 71428571;
-var tW = 47795; // total wallets or participants
+// Set the minimum and maximum percentage to chose randomly from for each of the groups
+const minP = 4.4
+const maxP = 9.09
+const rand = random.bind(null,minP,maxP)
+
+const airDrT = 71428571;
+var tW = 30000; // total wallets or participants, put any number you feel will be the total amount of registerd participants
 var tDDSS = 46780356295;
 var airDrA;
 var groups = [];
@@ -45,3 +49,7 @@ var factor = multi.reduce((p,c)=> p+c);
 airDrA = airDrT / factor
 //Airdrop coins forEach participant
 log(`Each participant gets: ${airDrA} (excluding bonus based on ddss)`)
+
+log(`Total to distribute: ${airDrA * tW} (excluding bonus based on ddss)`)
+
+log(`Total remaining for bonuses: ${airDrT - (airDrA * tW)}`)
